d9597c27018773d8b9616caec14c140aad4ce23e
Sakshi Gapat <158327493+SakshiGapat@users.noreply.github.com>
2025-07-30 23:07:42 +0530

    Reversing the number

---
+++ reversenu.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class reversenu{
+    int num;
+    //int n = x.length;
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number:");
+        int num = sc.nextInt();
+        int reverse = 0;
+    
+         while (num != 0) {
+            int digit = num % 10;        
+            reverse = reverse * 10 + digit; 
+            num = num / 10;
+         }
+          System.out.println("Reversed Number: " + reverse);
+    }
+}
