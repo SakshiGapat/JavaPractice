a8b3ca5f8d54da2f75930cca96f55423c416bd83
Sakshi Gapat <158327493+SakshiGapat@users.noreply.github.com>
2025-08-01 22:44:20 +0530

    Reversing Array

---
+++ reveringarray.java
@@ -0,0 +1,18 @@
+public class reveringarray {
+    public static void main(String[] args)
+     {
+        
+        int[] arr = {43,47,10,5,3,81};
+        int n = arr.length;
+
+        for(int j=n-1;j>=0;j--)
+        {
+            
+            
+            System.out.println(arr[j]);
+        }
+
+        }
+    }
+    
+
