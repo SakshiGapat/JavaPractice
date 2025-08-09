c9a5ec19f61e1a8c4950bcf20a08eff136369f39
Sakshi Gapat <158327493+SakshiGapat@users.noreply.github.com>
2025-08-09 20:53:13 +0530

    How many times people can shake hands

---
+++ Handshake.java
@@ -0,0 +1,11 @@
+//wipro in a room n people if everyone shakes hand with everyone once then how many times handshake will happen
+
+public class file7 {
+    public static void main(String[] args) {
+        
+        int n = 10;
+        int a = n*(n-1)/2;
+        System.out.println(a);
+    }
+    
+}
